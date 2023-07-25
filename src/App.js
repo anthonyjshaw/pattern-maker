822e77d0a3a7cd43c21409dea842ad1957f369ed
Anthony Shaw <anthonyshaw@sceaga.com>
2023-07-25 22:16:00 +0100

    FP-4 Added backward and forward arrows

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import TemplateOption from "./TemplateOption";
 import Tooltip from "./Tooltip";
 import SizeOptions from "./SizeOptions";
 import { PatternOptions } from "./PatternOptions";
+import Carousel from "./Carousel";
 
 const tShirt = require("./lib/pattern_options/t-shirt.json");
 const trousers = require("./lib/pattern_options/trousers.json");
@@ -21,21 +22,40 @@ const App = () => {
 		name: 't-shirt',
 		image_src: '/t-shirt.svg'
 	});
-
+	const [carouselOption, setCarouselOption ] = useState('Front');
 	const [patternOptionValues, setPatternOptionValues] = useState([
 		"t-shirt"
 	])
-
+	let carouselIndex = 0;
+	let carouselOptions = ['Front', 'Back', "Left Side", "Right Side", "Pieces"];
 	const patternOptionsArray = [tShirt, trousers, sweatshirt, dress, skirt, shirt, jacket].map(e => {
 		return <PatternOptions key={e.pattern_name} name={e.pattern_name} elements={e.elements} />
 	});
 
-	console.log(patternOptionsArray)
 	const patternOption = patternOptionsArray.find(e => e.props.name === picture.name)
 
+
 	
-	function handleClick(e) {
+	function handleClickPicture(e) {
 		setPicture({ name: e.target.dataset.imgSrc, image_src: e.target.src });
+	}
+
+	function handleClickCarouselForward(e) {
+		if (carouselIndex > carouselLength) {
+			carouselIndex = 0;
+		} else {
+			carouselIndex++;
+		}
+		setCarouselOption(carouselOptions[carouselIndex])
+	}
+
+	function handleClickCarouselBackward(e) {
+		if (carouselIndx < 0) {
+			carouselIndex = 0;
+		} else {
+			carouselIndex--;
+		}
+		setCarouselOption(carouselOptions[carouselIndex]);
 	}
 	const sizes = [
 		{
@@ -92,7 +112,7 @@ const App = () => {
 			name: 'shirt',
 			icon: '/shirt.svg'
 		}
-	].map(e => <TemplateOption icon={e.icon} text={e.name} className={'template-option-img'} alt={`SVG of ${e.name}`} onClick={handleClick} key={e.name} tooltip={<Tooltip text={e.name} />} />);
+	].map(e => <TemplateOption icon={e.icon} text={e.name} className={'template-option-img'} alt={`SVG of ${e.name}`} onClick={handleClickPicture} key={e.name} tooltip={<Tooltip text={e.name} />} />);
 	return (
 		<div>
 			<h1>Fashion app</h1>
@@ -105,7 +125,7 @@ const App = () => {
 						<img src={`${picture.image_src}`} alt={`Image of ${picture.name}`} className="picture-item" />
 					</div>
 					<div>
-						<h3>Front</h3>
+						<Carousel option={carouselOption}/>
 					</div>
 				</div>
 				<div className="side-panel">
--- src/Carousel.js
+++ src/Carousel.js
@@ -1,6 +1,10 @@
-const Carousel = () => {
+const Carousel = (props) => {
+	const option = props.option;
 	return (
-		<div></div>
+		<div>
+			<h3><span onClick={props.backward}>{"<"}</span>{option}<span onClick={props.forward}>{">"}</span></h3> 
+
+		</div>
 	);
 }
 
