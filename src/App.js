0ac379f78d9edc5d017dadd3ed8944020e19567b
Anthony Shaw <anthonyshaw@sceaga.com>
2023-07-25 23:22:55 +0100

    FP-4 Update picture to reflect useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "./Button";
 import TemplateOption from "./TemplateOption";
 import Tooltip from "./Tooltip";
@@ -17,24 +17,30 @@ const jacket = require("./lib/pattern_options/jacket.json");
 
 
 const App = () => {
-	const [picture, setPicture] = useState({
-		name: 't-shirt',
-		image_src: '/t-shirt.svg'
-	});
-	const [patternOptionValues, setPatternOptionValues] = useState([
+	useEffect(() => {
+		setCarouselOption(carouselOptions[carouselIndex]);
+	})
+	const [patternOptionValues, setPatternOptionValues] = useState(
 		"t-shirt"
-	])
+	)
 	const [carouselIndex, setCarouselIndex] = useState(0);
-	const carouselOptions = ['Front', 'Back', "Left Side", "Right Side", "Pieces"];
+	const carouselOptions = ['Front', 'Back', "Left Side", "Right Side"];
 	const patternOptionsArray = [tShirt, trousers, sweatshirt, dress, skirt, shirt, jacket].map(e => {
 		return <PatternOptions key={e.pattern_name} name={e.pattern_name} elements={e.elements} />
 	});
 	const [carouselOption, setCarouselOption] = useState(carouselOptions[carouselIndex]);
 
-	const patternOption = patternOptionsArray.find(e => e.props.name === picture.name)
+	const [picture, setPicture] = useState({
+		name: 't-shirt',
+		image_src: `/${carouselOption.toLowerCase()}/t-shirt.svg`
+	});
+	const patternOption = patternOptionsArray.find(e => e.props.name === patternOptionValues)
 
 	function handleClickPicture(e) {
-		setPicture({ name: e.target.dataset.imgSrc, image_src: e.target.src });
+		setPatternOptionValues(e.target.dataset.imgSrc)
+		setCarouselIndex(0);
+
+		setPicture({ name: e.target.dataset.imgSrc, image_src: `/front/${e.target.dataset.imgSrc}.svg` });
 	}
 
 	function handleClickCarouselForward(e) {
@@ -43,7 +49,7 @@ const App = () => {
 		} else {
 			setCarouselIndex(carouselIndex + 1)			
 		}
-		setCarouselOption(carouselOptions[carouselIndex])
+		setPicture({ name: e.target.dataset.imgSrc, image_src: `/${carouselOption.toLowerCase().replaceAll(' ', '-')}/${patternOptionValues}.svg` });
 	}
 
 	function handleClickCarouselBackward(e) {
@@ -52,7 +58,7 @@ const App = () => {
 		} else {
 			setCarouselIndex(carouselIndex - 1);
 		}
-		setCarouselOption(carouselOptions[carouselIndex]);
+		setPicture({ name: e.target.dataset.imgSrc, image_src: `/${carouselOption.toLowerCase().replaceAll(' ', '-')}/${patternOptionValues}.svg` });
 	}
 	const sizes = [
 		{
@@ -83,31 +89,31 @@ const App = () => {
 	const templateOptions = [
 		{
 			name: 't-shirt',
-			icon: "/t-shirt.svg"
+			icon: "/front/t-shirt.svg"
 		},
 		{
 			name: 'trousers',
-			icon: '/trousers.svg'
+			icon: '/front/trousers.svg'
 		},
 		{
 			name: 'sweatshirt',
-			icon: '/sweatshirt.svg'
+			icon: '/front/sweatshirt.svg'
 		},
 		{
 			name: 'jacket',
-			icon: '/jacket.svg'
+			icon: '/front/jacket.svg'
 		},
 		{
 			name: 'dress',
-			icon: '/dress.svg'
+			icon: '/front/dress.svg'
 		},
 		{
 			name: 'skirt',
-			icon: '/skirt.svg'
+			icon: '/front/skirt.svg'
 		},
 		{
 			name: 'shirt',
-			icon: '/shirt.svg'
+			icon: '/front/shirt.svg'
 		}
 	].map(e => <TemplateOption icon={e.icon} text={e.name} className={'template-option-img'} alt={`SVG of ${e.name}`} onClick={handleClickPicture} key={e.name} tooltip={<Tooltip text={e.name} />} />);
 	return (
